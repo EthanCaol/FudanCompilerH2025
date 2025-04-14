022493811a990dedaf6e557ccb1e5a840d8966ef
Ethan Cao <1677035769@qq.com>
2025-04-14 12:12:33 +0800

    25/4/14 12:12

--- HW5/tools/main/main.cc
+++ HW5/tools/main/main.cc
@@ -7,6 +7,7 @@
 
 #include "ASTheader.hh"
 #include "FDMJAST.hh"
+#include "ast2xml.hh"
 #include "xml2ast.hh"
 
 #include "config.hh"
@@ -44,21 +45,23 @@ int main(int argc, const char* argv[])
     string file_ast_semant = file + ".2-semant.ast.my";
     string file_irp = file + ".3.irp.my";
 
-    // cout << "从FMJ文件读取并解析AST: " << file_fmj << endl;
-    // ifstream fmjfile(file_fmj);
-    // fdmj::Program* root = fdmjParser(fmjfile, false);
-    // if (root == nullptr) {
-    //     cout << "AST无效" << endl;
-    //     return EXIT_FAILURE;
-    // }
+    cout << "从FMJ文件读取并解析AST: " << file_fmj << endl;
+    ifstream fmjfile(file_fmj);
+    fdmj::Program* root = fdmj::fdmjParser(fmjfile, false);
+    if (root == nullptr) {
+        cout << "AST无效" << endl;
+        return EXIT_FAILURE;
+    }
 
-    // cout << "将AST转换为XML文件: " << file_ast << endl;
-    // XMLDocument* x = ast2xml(root, nullptr, with_location_info, false); // no semant info yet
-    // x->SaveFile(file_ast.c_str());
-    // if (x->Error()) {
-    //     cout << "AST无效" << endl;
-    //     return EXIT_FAILURE;
-    // }
+    cout << "将无语法AST保存为XML文件: " << file_ast << endl;
+    XMLDocument* x = ast2xml(root, nullptr, with_location_info, false); // no semant info yet
+    x->SaveFile(file_ast.c_str());
+    if (x->Error()) {
+        cout << "AST无效" << endl;
+        return EXIT_FAILURE;
+    }
+
+    
 
     // cout << "从XML文件读取AST: " << file_ast << endl;
     // AST_Semant_Map* semant_map = new AST_Semant_Map();
