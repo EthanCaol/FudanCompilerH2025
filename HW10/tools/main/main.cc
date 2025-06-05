40e08c2137079ed190ff2a22d8efcc25d91940d6
Ethan Cao <1677035769@qq.com>
2025-06-05 16:27:51 +0800

    25/6/5 16:27

---
+++ HW10/tools/main/main.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <string>
+#include <filesystem>
+#include <unistd.h>
+
+#include "treep.hh"
+#include "quad.hh"
+#include "quad2xml.hh"
+#include "xml2quad.hh"
+#include "opt.hh"
+
+using namespace std;
+using namespace tree;
+using namespace quad;
+using namespace tinyxml2;
+
+int main(int argc, const char* argv[])
+{
+    // 切换到test目录
+    filesystem::path filePath(__FILE__);
+    filesystem::path directory = filePath.parent_path();
+    chdir(directory.c_str());
+    chdir("../../test/input_example");
+
+    string file;
+    file = argv[argc - 1];
+    file = "hw10test00";
+    // file = "bubblesort";
+
+    // boilerplate output filenames (used throughout the compiler pipeline)
+    string file_quad_ssa_xml = file + ".4-ssa-xml.quad";
+    string file_quad_ssa = file + ".4-ssa.txt";
+    string file_quad_ssa_opt = file + ".4-zopt.txt";
+
+    cout << "读取quad: " << file_quad_ssa_xml << endl;
+    quad::QuadProgram* x3 = xml2quad(file_quad_ssa_xml.c_str());
+    if (x3 == nullptr) {
+        cerr << "Error reading Quad from xml: " << file_quad_ssa_xml << endl;
+        return EXIT_FAILURE;
+    }
+
+    cout << "Writing is back (text) to: " << file_quad_ssa << endl;
+    quad2file(x3, file_quad_ssa, true);
+    QuadProgram* x4 = optProg(x3); // using registers to prepare the quad for RPI
+    
+    cout << "Writing the optimized Quad Program to: " << file_quad_ssa_opt << endl;
+    quad2file(x4, file_quad_ssa_opt.c_str(), true); // write the prepared quad to a file
+    
+    cout << "-----Done---" << endl;
+    return EXIT_SUCCESS;
+}
